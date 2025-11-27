437d015e8788121a9dfbcd0e7f361fd06d315c7b
Rio Flores <rio.1.flor@gmail.com>
2025-11-26 19:29:52 -0500

    deleted prototypes that are on hard drive

--- image + constraint/Assets/Scripts/DadController.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class DadController : MonoBehaviour
-{
-    public GameObject Set1, Set2, Set3;
-    
-}
--- image + constraint/Assets/Scripts/SceneChanger.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class SceneChanger : MonoBehaviour
-{
-    void Update()
-    {
-        SceneManager.LoadScene("SampleScene");
-    }
-}
